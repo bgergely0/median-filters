d6272abdeeb310dd956797d4f175bda13954133d
Gergely Bálint <gergely@balintgergely.hu>
2023-01-07 17:43:57 +0100

    fix CPU implementation

--- cpu/generate_indexes.py
+++ cpu/generate_indexes.py
@@ -8,17 +8,12 @@ while p < n:
     k = p
     while k >= 1:
         j = k % p
-        while j < n - k:
+        while j < n - k - 1:
             i = 0
             while i < k:
                 if math.floor(float(i+j)/float(2*p))==math.floor(float(i+j+k)/float(2*p)):
                     if (i+j+k < 25):
-                        #print("\t\t\tsort_two_vectors(filterbuffer["+ str(int(i+j+k))+"],filterbuffer["+str(int(i+j))+"], tmp); // sort #" + str(sortnum))
-                        sort_idx_1_x = int(i+j+k)//5
-                        sort_idx_1_y = int(i+j+k)%5
-                        sort_idx_2_x = int(i+j)//5
-                        sort_idx_2_y = int(i+j)%5
-                        print("\t\t\tsort_two_vectors(shmem["+ str(sort_idx_1_x)+"]["+ str(sort_idx_1_y) +"], shmem["+ str(sort_idx_2_x)+"]["+ str(sort_idx_2_y) +"], tmp); // sort #" + str(sortnum))
+                        print("\t\t\tsort_two_vectors(filterbuffer["+ str(int(i+j+k))+"],filterbuffer["+str(int(i+j))+"], tmp); // sort #" + str(sortnum))
                         sortnum += 1
                 i = i+1
             j = j + 2*k
--- cpu/median_filter.cpp
+++ cpu/median_filter.cpp
@@ -11,67 +11,56 @@ void median_filter(int imgHeight, int imgWidth, int imgWidthF,
 		{
 
 			short output_pixel[3] = { 0,0,0 };
-			short filterbuffer_r[32] = {0};
-			short filterbuffer_g[32] = {0};
-			short filterbuffer_b[32] = {0};
+			short buffer[75] = {0};
 
-			for (int fy = 0; fy < FILTER_H; fy++)
+			for (int rgb = 0; rgb < 3; rgb++)
 			{
-				for (int fx = 0; fx < FILTER_W; fx++)
+				for (int fy = 0; fy < FILTER_H; fy++)
 				{
-					int filter_idx = fy * FILTER_W + fx;
-					//5x5 filter filled with elements at every pixel
-					filterbuffer_r[filter_idx]
-						= imgSrcExt[((row + fy) * imgWidthF + col + fx) * 3 + 0];
-
-					filterbuffer_g[filter_idx]
-						= imgSrcExt[((row + fy) * imgWidthF + col + fx) * 3 + 1];
-
-					filterbuffer_b[filter_idx]
-						= imgSrcExt[((row + fy) * imgWidthF + col + fx) * 3 + 2];
+					for (int fx = 0; fx < FILTER_W; fx++)
+					{
+						int filter_idx = (fy * FILTER_W + fx) * 3 + rgb;
+						
+						// fill 5x5 filter input
+						buffer[filter_idx]
+							= imgSrcExt[((row + fy) * imgWidthF + col + fx) * 3 + rgb];
+					}
 				}
 			}
+			
 
-			//int n = FILTER_W * FILTER_H;
 			int n = 32;
 
-			for(int p = 1; p < n; p = p*2) {
-				for(int k = p; k >= 1; k = k/2) {
-					for(int j = k % p; j < n-k; j += 2*k) {
-						for(int i = 0; i < k; i++){
-							if( ((i+j) / (2*p)) == ((i+j+k) / (2*p)) ){ // floor op is implicit for integer types
-									// RED
-									if (filterbuffer_r[i + j] > filterbuffer_r[i + j + k]) {
-										short helper = filterbuffer_r[i + j];
-										filterbuffer_r[i + j] = filterbuffer_r[i + j + k];
-										filterbuffer_r[i + j + k] = helper;
+			for (int rgb = 0; rgb < 3; rgb++)
+			{
+				for(int p = 1; p < n; p = p*2) {
+					for(int k = p; k >= 1; k = k/2) {
+						for(int j = k % p; j < n-k-1; j += 2*k) {
+							for(int i = 0; i < k; i++){
+								if( ((i+j) / (2*p)) == ((i+j+k) / (2*p)) ){ // floor op is implicit for integer types
+									if( i + j + k < 25) // dont index out of the array
+									{
+										// sort i+j, i+j+k
+										int a = (i + j)*3 + rgb;
+										int b = (i + j + k)*3 + rgb;
+
+										if (buffer[a] > buffer[b]) {
+											short helper = buffer[a];
+											buffer[a] = buffer[b];
+											buffer[b] = helper;
+										}
 									}
-									// GREEN
-									if (filterbuffer_g[i + j] > filterbuffer_g[i + j + k]) {
-										short helper = filterbuffer_g[i + j];
-										filterbuffer_g[i + j] = filterbuffer_g[i + j + k];
-										filterbuffer_g[i + j + k] = helper;
-									}
-									// BLUE
-									if (filterbuffer_b[i + j] > filterbuffer_b[i + j + k]) {
-										short helper = filterbuffer_b[i + j];
-										filterbuffer_b[i + j] = filterbuffer_b[i + j + k];
-										filterbuffer_b[i + j + k] = helper;
-									}
+								}
 							}
 						}
 					}
 				}
 			}
 
-			output_pixel[0] = filterbuffer_r[13+7];
-			output_pixel[1] = filterbuffer_g[13+7];
-			output_pixel[2] = filterbuffer_b[13+7];
-
 			// Write to output picture
 			for (int rgb = 0; rgb < 3; rgb++)
 			{
-				imgDst[(row * imgWidth + col)*3 + rgb] = output_pixel[rgb];
+				imgDst[(row * imgWidth + col)*3 + rgb] = buffer[12*3 + rgb];
 			}
 		}
 	}
